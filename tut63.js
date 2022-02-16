9b166aac15ac1743a3a8b8324c1f133ea75c8775
chetan4java <chetan.badgujar@gmail.com>
2022-02-16 16:55:03 +0530

    63.js changes

--- tut63.js
+++ tut63.js
@@ -7,11 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
  // res.end('Hello World this is chetan badgujar ');
-  res.end(`<style>
-          @keyframes spin {from {} to {transform:rotate(360deg);}}
-          .spin:hover {animation-name:spin;animation-duration:3s;position:absolute;}
-      </style>
-      <br><br><span class="spin">Hover To Spin!</span><a target="_blank" href="http://free-web-tools.com" style="text-decoration:none;bottom:0;left:10px;font-size:5pt;color:gray;position:absolute">free web tools</a><a target="_blank" href="http://www.htmlfreecodes.com" style="font-size: 8pt; text-decoration: none">Html Codes</a> `);
+  res.end("hello from 63.js");
 });
 
 server.listen(port, hostname, () => {
